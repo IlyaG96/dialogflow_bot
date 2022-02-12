6ef526ec495c8f927dd264995e86ba85958a7e14
IlyaG96 <ilyag_96@icloud.com>
2022-02-12 20:47:22 +1100

    add vk bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,10 +1,28 @@
 from environs import Env
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
+
+def run_vk_bot(vk_token):
+
+    vk_session = vk_api.VkApi(token=vk_token)
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
 
 def main():
     env = Env()
     env.read_env()
     vk_token = env.str('VK_TOKEN')
+    run_vk_bot(vk_token)
 
 if __name__ == '__main__':
     main()
